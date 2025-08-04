a47848ea734c89bc499b220d5c9df2c4c4b9a375
Ger Hobbelt <ger@hobbelt.com>
2025-08-04 20:15:55 +0200

    added source file for the main GoolgeTest test runner/driver (monolithic build mode)

---
+++ sources/tests/test-runner-main.cpp
@@ -0,0 +1,29 @@
+//
+// main() for monolithic build of the libADE tests
+//
+
+#include "gtest/gtest.h"
+
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+using ::testing::InitGoogleTest;
+using ::testing::TestEventListeners;
+using ::testing::TestWithParam;
+using ::testing::UnitTest;
+using ::testing::Test;
+using ::testing::Values;
+
+
+int main(int argc, const char** argv)
+{
+	int rv = 0;
+
+	InitGoogleTest(&argc, argv);
+
+	rv = RUN_ALL_TESTS();
+
+	return rv;
+}
